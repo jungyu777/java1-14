69ee19010c738f5204ed2e6e2a30360a6b80e147
jun <itpr2003@naver.com>
2023-05-12 18:28:28 +0900

    추가

---
+++ Step17_JDBC/src/test/main/MainClass01.java
@@ -0,0 +1,61 @@
+package test.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/*
+ * JDBC( java DataBase Connectivity )
+ * 
+ * DataBase 에 연결해서 SELECT , INSERTM UPDATE, DELETE 작업하기
+ * 
+ * Oracle 에 연결하기 위해서는 드라이버 클래스가 들어있는 ojdbc6.jar 파일을
+ * 사용할수 있도록 설정해야한다.
+ * module info java 삭제
+ * 프로젝트에 마우스 우클릭=> Build Path = > Configure Build path=> Librarys 탭선택
+ * => classpath 선택 => 우측 Add External jar 버튼을 누른후 =>  ojdbc6.jar 팡ㄹ을 찾은 다음 =>Apply
+ */
+public class MainClass01 {
+	public static void main(String[] args) {
+		   //DB 연결객체를 담을 지역 변수 만들기
+	      Connection conn=null;
+	      
+	      try {
+	         //오라클 드라이버 로딩
+	         Class.forName("oracle.jdbc.driver.OracleDriver");
+	         //접속할 DB 의 정보 @아이피주소:port번호:db이름
+	         String url="jdbc:oracle:thin:@localhost:1521:xe";
+	         //계정 비밀번호를 이용해서 Connection 객체의 참조값 얻어오기
+	         conn=DriverManager.getConnection(url, "scott", "tiger");
+	         //예외가 발생하지 않고 여기까지 실행순서가 내려오면 접속 성공이다.
+	         System.out.println("Oracle DB 접속 성공");
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }
+	      //SeLECT 작업을 위해서 필요한 객체의 참조값을 담을 지역 변수 미리 만들기
+	      PreparedStatement pstmt = null;
+	      //SELECT 문 실행하고 결과 값을 ResultSet 으로 얻어내기
+	      ResultSet rs=null;
+	      try {
+	    	  //실행할 sql 문
+	    	  String sql = "SELECT num,name,addr"
+	    			     +" FROM member"
+	    			     +" ORDER BY num DESC";
+	    	  pstmt=conn.prepareStatement(sql);
+	    	  rs=pstmt.executeQuery();
+	    	  while(rs.next()) {//rs.next()커서이동
+	    		  int num=rs.getInt("num");
+	    		  String name = rs.getString("name");
+	    		  String addr = rs.getString("addr");
+	    		  //콘솔창에 출력해보기
+	    		  System.out.println(num + "|" +name +"|" + addr);
+	    	  }
+	      }catch(Exception e) {
+	    	  e.printStackTrace();
+	      }
+		
+	}
+	
+	
+}
